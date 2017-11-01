e55fd29ffb6935fbf173f57c57f8618eec3bef9b
Ram Gopinath <RGopinath@coloradogroup.com.au>
2017-11-01 13:39:57 +1100

    label alignemnts

--- resources/views/labels/templates/cartonloose.blade.php
+++ resources/views/labels/templates/cartonloose.blade.php
@@ -3,25 +3,33 @@
 		@foreach ($carton['carton_details'] as $details)
 			^XA
 				^FX Top section.
-				^CF0,70
+				^CF0,50
 				^FO90,50^FDOrder No: {{$carton['order_number']}}^FS
 				^FO90,120^FDStyle No: {{$carton['style']}}^FS
 				^FO90,180^FDSize: {{$carton['item_size']}}^FS
-				^FO90,250^FDColour: {{$carton['colour']}}^FS
+				^FO90,250^FH^FDColour: {{str_replace('~',' _7e ',$carton['colour'])}}^FS
 				^FO90,320^FDQty: {{$carton['quantity']}}^FS
 
 				^FX Second.
-				^CFA,30
+				^CF0,30
 				^FO50,400^FDItem No  {{$carton['item']}}^FS
-				^FO50,450^FDDescription   {{$carton['description']}}^FS
+				^FO50,450^FH^FDDescription   {{str_replace('~',' _7e ',$carton['description'])}}^FS
 
 				^FX Third section with barcode.
-				^BY3.4,2,230
-				^FO50,550^BC^FD{{$details['number']}}^FS
+				^BY3,2,230
+				^FO30,550^BCN,150,N,N,Y
+				^FD{{$details['barcode']}}^FS
+				^FO30,720
+				^A0N,50,40
+				^FD{{$details['number']}}^FS
 
 				^FX Fourth section (the two boxes on the bottom).
-				^BY2,2,230
-				^FO50,850^BC^FD{{$carton['product_indicator_number']}} 1^FS
+				^BY2,2,150
+				^FO30,850^BCN,150,N,N,Y
+				^FD{{$carton['product_indicator_barcode']}}^FS
+				^FO30,1020
+				^A0N,50,40
+				^FD{{$carton['product_indicator_number']}}^FS
 			^XZ
 			<br/>
 		@endforeach	
--- resources/views/labels/templates/cartonpack.blade.php
+++ resources/views/labels/templates/cartonpack.blade.php
@@ -4,28 +4,38 @@
 			^XA
 
 			^FX Top section.
-			^CF0,70
+			^CF0,50
 			^FO90,50^FDOrder No: {{$data['order_number']}}^FS
 			^FO90,110^FDStyle No:{{$data['style']}}^FS
 			^FO90,170^FDPack Item:{{$data['packnumber']}}^FS
-			^CF0,340
+			^CF0,300
 			^FO580,30^FD{{$data['packtype']}}^FS
 
 			^FX Second.
-			^CFA,30
-			^FO50,300^FDDescription   {{$data['description']}}^FS
+			^CF0,40
+			^FO50,300^FH^FDDescription   {{$data['description']}}^FS
 			^FO50,340^FDGroup  {{$data['group']}}^FS
 			^FO50,380^FDDept  {{$data['dept']}}^FS
 			^FO50,420^FDClass  {{$data['class']}}^FS
-			^FO50,460^FDClass  {{$data['subclass']}}^FS
+			^FO50,460^FDSubClass  {{$data['subclass']}}^FS
 
 			^FX Third section with barcode.
-			^BY3.4,2,230
-			^FO50,550^BC^FD{{$carton['number']}}^FS
+			^BY3,2,230
+			^FO50,550
+			^BCN,150,N,N,Y
+			^FD{{$carton['barcode']}}^FS
+			^FO50,720
+			^A0N,50,40
+			^FD{{$carton['number']}}^FS
 
 			^FX Fourth section (the two boxes on the bottom).
-			^BY2,2,230
-			^FO50,850^BC^FD{{$data['product_indicator_number']}}^FS
+			^BY2,2,150
+			^FO50,850
+			^BCN,150,N,N,Y
+			^FD{{$data['product_indicator_barcode']}}^FS
+			^FO50,1020
+			^A0N,50,40
+			^FD{{$data['product_indicator_number']}}^FS
 			^XZ
 			<br/>
 		@foreach	
