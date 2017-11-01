ca0247f9d8dfc4a49a01fe0cc5e1b274a3b3e670
Ram Gopinath <RGopinath@coloradogroup.com.au>
2017-11-01 13:43:34 +1100

    print and archive files seperation

--- app/UserLabelPrint.php
+++ app/UserLabelPrint.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Scopes\UserScope;
+use Carbon\Carbon;
 
 class UserLabelPrint extends Model
 {
@@ -27,4 +28,37 @@ class UserLabelPrint extends Model
     protected $fillable = [
         'type', 'raw_data', 'printed', 'user_id', 'quantity', 'creator'
     ];
+
+    /**
+     * Get the user label print updated at field.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getUpdatedAtAttribute($value)
+    {
+    	return Carbon::parse($value)->toDayDateTimeString();
+    }
+
+    /**
+     * Scope print.
+     *
+     * @param  string  $query
+     * @return string
+     */
+    public function scopePrint($query)
+    {
+        return $query->where('printed','0');
+    }
+
+    /**
+     * Scope Archived.
+     *
+     * @param  string  $query
+     * @return string
+     */
+    public function scopeArchived($query)
+    {
+        return $query->where('printed','1');
+    }    
 }
